f14d57a3862bd35ed2d886c6ada9ad6d4d3cd0f3
Kyle Samson <kylesamson21@gmail.com>
2015-01-27 20:00:14 -0400

    fooling around

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Query String - Challenge 3.2</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="stylesheet.css" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div id="form" class="container col-md-16">
+  <h2><?php echo "Query String Challenge"; ?></h2>
+  <form role="form" method="get" action="handler-challenge-3-2.php">
+      <input id="submit1" type="submit" name="submit1" class="btn btn-danger" value="This is link number one.">
+      <input id="submit2" type="submit" name="submit2" class="btn btn-danger" value="This is link number two.">
+      <input id="submit3" type="submit" name="submit3" class="btn btn-danger" value="This is link number three.">
+  </form>
+</div>
+</body>
+</html>
